93b4a4b12286f2b846e86a26fe5f70c605f9ba21
GangiReddy Pole <117641645+Reddy4175@users.noreply.github.com>
2023-02-15 21:35:10 +0530

    val

--- my-app/src/component/Compclass.jsx
+++ my-app/src/component/Compclass.jsx
@@ -16,19 +16,3 @@ class Compclass extends React.Component{
     }
 }
 export default Compclass
-// import React, {Component} from "react";
-// import Compclass2 from "./Compclass2";
-// class Compclass extends Component{
-//     name="oe"
-//     email="new@gmail.com"
-//     render(){
-//         return(
-//             <div>
-//                 <h4>
-//                     <Compclass2 name={this.name} email={this.email}/>
-//                 </h4>
-//             </div>
-//         )
-//     }
-// }
-//export default Compclass
